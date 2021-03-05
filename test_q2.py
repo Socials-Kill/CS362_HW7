bc49ed561b4e407a7c8358b56ebab5fa550d7095
BuildTools <unconfigured@null.spigotmc.org>
2021-03-04 21:36:16 -0800

    Q2 Wrote Second Test, divisible by 100

--- test_q2.py
+++ test_q2.py
@@ -5,5 +5,7 @@ class yearTest(unittest.TestCase):
     def test_four(self):
         self.assertEqual(q2.leapYear(4), "This is a leap year.")
     
+    def test_hundred(self):
+        self.assertEqual(q2.leapYear(100), "This is not a leap year.")
 if __name__ == "__main__":
     unittest.main(verbosity = 2)
