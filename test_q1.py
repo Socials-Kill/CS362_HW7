df5600ae57c52c770d1a8f8a4c066656a888b9ad
BuildTools <unconfigured@null.spigotmc.org>
2021-03-04 20:58:56 -0800

    Q1 Wrote Second Test, Buzz

--- test_q1.py
+++ test_q1.py
@@ -5,6 +5,8 @@ class fizzTest(unittest.TestCase):
     def test_fizz(self):
         self.assertEqual(q1.count(), "fizz")
     
+    def test_buzz(self):
+        self.assertEqual(q1.count(), "buzz")
 
 if __name__ == "__main__":
     unittest.main(verbosity = 2)
