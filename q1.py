0c1afa59a05b7c94da80770af536a4b6b615221e
BuildTools <unconfigured@null.spigotmc.org>
2021-03-04 21:01:10 -0800

    Q1 Implemented count() method, factor of 3 and 5

--- q1.py
+++ q1.py
@@ -1,8 +1,10 @@
 def count():
     for x in range (1,101):
-        if x % 3 == 0:
+        if x % 3 == 0 and x % 5 == 0:
+            return "fizzbuzz"
+        elif x % 3 == 0:
             print("fizz")
         elif x % 5 == 0:
-            return "buzz"
+            print("buzz")
         else:
             print(x)
